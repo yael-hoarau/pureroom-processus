089668b69065ff3a818e3e916c7b09ba9ac52383
Yaël Hoarau <yael.hoarau@etu.univ-amu.fr>
2022-02-15 16:53:54 +0100

    Change port app to 8080

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 8080
 
 //const {InfluxDB, FluxTableMetaData} = require('@influxdata/influxdb-client')
 const {url, token, org} = require('./env')
